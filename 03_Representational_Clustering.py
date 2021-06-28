705f3d844c9484edaeede6343f17133bf5309342
Jordan Gunn <gunnjordanb@gmail.com>
2021-06-28 16:13:10 -0500

    June 28, 2021 4:13 PM

--- 03_Representational_Clustering.py
+++ 03_Representational_Clustering.py
@@ -1,7 +1,98 @@
 # %% [markdown]
-# ## Simulation Configuration
-# Let's demonstrate how to efficiently simulate the Landscape Model using the stimuli from our SBS dataset.
+# # Clustering by Representational Similarity
+
+# %% [markdown]
+# Previous work has applied a distance rank analysis to summarize with a single scalar value the tendency to group together nearby items using various distance metrics, including serial order and semantic similarity. This analysis is also probably applicable to measure the extent how recall is clustered according to latent representational states inferred with our models. For example, the distance_rank analysis can be applied to data using semantic similarities from GloVe, but also to semantic connections simulated with the Landscape model.
+#
+# To really underline how dynamics within the Landscape model progressively _evolve_ a representation of semantic associations between items, we can simulate the study phase of each trial using the model's default parameters and track this distance_rank statistic at each increment. A horizontal line records the initial value based on pre-existing semantic associations, also the default matrix used for SemanticCMR and associated analyses.
+
+# %% [markdown]
+# ## Load Relevant Dependencies and Data
+# For flexibility, we'll retrieve our own similarities.
 
 # %%
 from Landscape_Model import LandscapeRevised
+from sentence_transformers import SentenceTransformer, util
+import pandas as pd
+import numpy as np
+import json
+from psifr import fr
+import spacy
+
+# load recall data frame
+data = pd.read_csv('data/psifr_sbs.csv')
+events = fr.merge_free_recall(
+    data, list_keys=['item_index', 'cycle', 'story_index', 
+                     'story_name', 'time_test'])
+
+# paraphrase-MiniLM-L12-v2
+# average_word_embeddings_glove.6B.300d
+# average_word_embeddings_glove.840B.300d
+# stsb-distilbert-base
+model = SentenceTransformer('average_word_embeddings_glove.840B.300d')
+units = events.pivot_table(index=['story_name', 'input'], values='item', aggfunc='first').reset_index()
+connections = {}
+remove_stopwords = False
+nlp = spacy.load('en_core_web_sm')
+
+for story_name in ['Fisherman', 'Supermarket', 'Flight', 'Cat', 'Fog', 'Beach']:
+    
+    sentences = units.loc[units.story_name==story_name].item.values.tolist()
+    
+    clean_sentences = []
+    for i in range(len(sentences)):
+        if remove_stopwords:
+            text_token = nlp(sentences[i])
+            clean_sentences.append(' '.join([word.text for word in text_token if not word.is_stop]))
+        else:
+            clean_sentences.append(sentences[i])
+    
+    #Compute embeddings
+    embeddings = model.encode(clean_sentences, convert_to_tensor=True)
+
+    #Compute cosine-similarities for each sentence with each other sentence
+    cosine_scores = np.abs(util.pytorch_cos_sim(embeddings, embeddings).numpy())
+    cosine_scores[np.eye(len(cosine_scores), dtype='bool')] = np.nan
+    connections[story_name] = cosine_scores
+
+events.head()
+
+# %% [markdown]
+# ## Demo Representational Clustering Analysis for Initial Model State
+# For each story and time_test, initialize the model with the relevant connectivity matrix, perform the lag_rank analysis over the dataset using the matrix, combine dataFrames, and plot the result.
+
+# %%
+
+distance_ranks = []
+
+# build list of distance_rank dfs across each factor i'm interested
+for time_test in pd.unique(events.time_test):
+    for story_name in pd.unique(events.story_name):
+
+        # initialize the model with the relevant connectivity matrix
+        model = LandscapeRevised(connections[story_name])
+        model.connections[np.eye(model.unit_count, dtype='bool')] = 0
+
+        # perform the distance_rank analysis over the dataset using the matrix
+        distance_rank = fr.distance_rank(
+            events.loc[(events.story_name==story_name) & (events.time_test==time_test)], 
+            'item_index', 1-model.connections).reset_index()
+
+        distance_rank['story_name'] = story_name
+        if time_test == 1:
+            distance_rank['time_test'] = 1
+        elif time_test == 2:
+            distance_rank['time_test'] = 'immediate'
+        else:
+            distance_rank['time_test'] = 'delayed'
+
+        distance_ranks.append(distance_rank)
+        
+distance_rank = pd.concat(distance_ranks)
+distance_rank = distance_rank.loc[distance_rank.time_test != 1]
+distance_rank
+
+# %% [markdown]
+# **Note**: Some of these rank values are nan for a given subject and condition. I need to understand why. One hypothesis? Participants didn't recall anything during these particular trials. This is because
+
 # %%
